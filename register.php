b9f777269fe9403846c0e392fc8afaf4dbe99160
Revifaturahman <123242308+Revifaturahman@users.noreply.github.com>
2024-09-01 13:27:42 +0700

    Update register.php

--- register.php
+++ register.php
@@ -205,8 +205,24 @@
     </div>
 
     <main class="form-signin w-100 m-auto">
-      <form action="feed.php">
-        <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+    <?php
+        echo "<div class='alert alert-primary text-center' role='alert'>
+          ". $_GET['msg'] ."
+        </div>";
+      ?>
+      <form action="process/pro-register.php" method="POST">
+        <h1 class="h3 mb-3 fw-normal text-center">Register</h1>
+
+        <div class="form-floating">
+          <input
+            type="text"
+            class="form-control"
+            id="floatingInput"
+            placeholder="username"
+            name="username";
+          />
+          <label for="floatingInput">Username</label>
+        </div>
 
         <div class="form-floating">
           <input
@@ -214,20 +230,35 @@
             class="form-control"
             id="floatingInput"
             placeholder="name@example.com"
+            name="email";
           />
           <label for="floatingInput">Email address</label>
         </div>
+
         <div class="form-floating">
           <input
             type="password"
             class="form-control"
             id="floatingPassword"
             placeholder="Password"
+            name="password";
           />
           <label for="floatingPassword">Password</label>
         </div>
+
+        <div class="form-floating">
+          <input
+            type="password"
+            class="form-control"
+            id="floatingPassword"
+            placeholder="Password"
+            name="confirm_password"
+          />
+          <label for="floatingPassword">Ulangi Password</label>
+        </div>
+
         <button class="btn btn-primary w-100 py-2" type="submit">
-          Sign in
+          Sign in 
         </button>
       </form>
     </main>
